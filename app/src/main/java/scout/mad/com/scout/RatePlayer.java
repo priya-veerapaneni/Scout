bbaa6a2c15ecacecdadff365b7f313fdb49a0abd
priya9999 <36176490+priya9999@users.noreply.github.com>
2019-09-23 11:30:42 -0700

    Initial commit

---
+++ app/src/main/java/scout/mad/com/scout/RatePlayer.java
@@ -0,0 +1,13 @@
+package scout.mad.com.scout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RatePlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate_player);
+    }
+}
